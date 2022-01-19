5fe78ade078c4ffaebaa5f197a8e9ba8c63a144d
Tayyab Aman <95743660+TAYYAB-IT@users.noreply.github.com>
2022-01-19 23:12:53 +0500

    Add files via upload

---
+++ np_2.py
@@ -0,0 +1,32 @@
+import numpy as np
+#Join arrays
+#arr1=np.array([1,2,3])
+#arr2=np.array([4,5,6])
+#print(np.concatenate((arr1,arr2))) #You to pass a tuple of arrays
+#arr1=np.array([[1,2,3],[4,5,6]])
+#arr2=np.array([[7,8,9],[10,11,12]])
+#print(np.concatenate((arr1,arr2),axis=0))
+#print(np.concatenate((arr1,arr2),axis=1))
+#arr1=np.array([1,2,3])
+#arr2=np.array([4,5,6])
+#print(np.stack((arr1,arr2),axis=0))
+#print(np.stack((arr1,arr2),axis=1))
+#arr1=np.array([[1,2,3],[4,5,6]])
+#arr2=np.array([[7,8,9],[10,11,12]])
+#print(np.stack((arr1,arr2),axis=0))
+#print(np.stack((arr1,arr2),axis=1))
+#print(np.stack((arr1,arr2),axis=2))
+#########Split_Arrays
+#arr1=np.array([0,1,2,3,4,5])
+#arr2=np.array([6,7,8,9,10,11])
+#print(np.array_split(arr1,3))
+#arr = np.array([1, 2, 3, 4, 5, 6])
+#newarr = np.array_split(arr, 4)
+#print(newarr)
+#print(newarr[0])
+#print(newarr[1])
+#print(newarr[2])
+#arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+#newarr = np.array_split(arr, 3)
+
+print(newarr)
